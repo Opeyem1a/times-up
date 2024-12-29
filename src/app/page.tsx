a418dbf251621de97418e8a8318421db21aa234e
opeyem1a <opeyadeyemi@gmail.com>
2024-12-28 22:46:32 -0700

    basic times-up functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,58 +1,129 @@
 'use client';
-import { useSearchParams } from 'next/navigation';
-import { useEffect, useState } from 'react';
 
-type SlideTime = { name: string; time: number };
+import { Dispatch, SetStateAction, useEffect, useMemo, useState } from 'react';
+import {
+    ExpectedSearchParams,
+    useSlideDataFromSearchParams,
+} from '@/app/hooks/useSlideDataFromSearchParams';
+
 export default function Home() {
-    const searchParams = useSearchParams();
-    const json_string = searchParams.get('data') ?? '{}';
-    const slides_json: [string, string][] = Object.entries(
-        JSON.parse(json_string)
-    );
-    const slides_obj_arr: SlideTime[] = [];
-    for (const [name, time] of slides_json) {
-        slides_obj_arr.push({ name, time: parseFloat(time) });
-    }
-    const warning_time = parseFloat(searchParams.get('warning') ?? '0');
+    const { config, warningTimeoutSeconds } = useSlideDataFromSearchParams();
 
-    const [slide_index, set_slide_index] = useState(0);
-    console.log(slide_index);
-    const [current_time, set_current_time] = useState(
-        slides_obj_arr[0].time * 60
+    // todo: validate warning timeout as well
+    if (config === null) return null;
+
+    console.log({ config });
+
+    return (
+        <HomeWithValidData
+            config={config}
+            warningTimeoutSeconds={warningTimeoutSeconds}
+        />
     );
+}
+
+const HomeWithValidData = ({
+    config,
+    warningTimeoutSeconds,
+}: ExpectedSearchParams) => {
+    const totalDuration = config.reduce(
+        (acc, curr) => acc + curr.durationInSeconds,
+        0
+    );
+    const [totalSecondsRemaining, setTotalSecondsRemaining] =
+        useState(totalDuration);
+
+    const currentSlide = useMemo(() => {
+        let potentialDurationAccumulated = 0;
+        for (const entry of config) {
+            potentialDurationAccumulated += entry.durationInSeconds;
+            if (
+                totalSecondsRemaining >
+                totalDuration - potentialDurationAccumulated
+            ) {
+                return entry;
+            }
+        }
+
+        return null;
+    }, [config, totalSecondsRemaining]);
+
+    const secondsToNextSlide = useMemo(() => {
+        const reversedConfig = [...config].reverse();
+
+        let counter = 0;
+        for (const entry of reversedConfig) {
+            counter += entry.durationInSeconds;
+            if (totalSecondsRemaining <= counter) {
+                return (
+                    totalSecondsRemaining - (counter - entry.durationInSeconds)
+                );
+            }
+        }
+
+        return 0;
+    }, [config, totalSecondsRemaining]);
+
+    if (currentSlide === null) {
+        return <div>COMPLETE</div>;
+    }
+
+    return (
+        <Countdown
+            currSecondsRemaining={totalSecondsRemaining}
+            currSlideSecondsRemaining={secondsToNextSlide}
+            currSlideName={currentSlide.name}
+            setTotalSecondsRemaining={setTotalSecondsRemaining}
+        />
+    );
+};
+
+type CountdownProps = {
+    currSecondsRemaining: number;
+    currSlideSecondsRemaining: number;
+    currSlideName: string;
+    setTotalSecondsRemaining: Dispatch<SetStateAction<number>>;
+};
+const Countdown = ({
+    currSecondsRemaining,
+    currSlideSecondsRemaining,
+    currSlideName,
+    setTotalSecondsRemaining,
+}: CountdownProps) => {
     useEffect(() => {
         const interval = setInterval(
-            () => set_current_time((prev) => prev - 1),
+            () => setTotalSecondsRemaining((prev) => prev - 1),
             1000
         );
+
         return () => {
             clearInterval(interval);
         };
     }, []);
 
-    useEffect(() => {
-        if (current_time <= -1) {
-            set_slide_index((curr) => curr + 1);
-            set_current_time(slides_obj_arr[slide_index + 1].time * 60);
-        }
-    }, [current_time]);
-
-    return (
-        <>
-            <p>hello</p>
-            <Display current_time={current_time} slide_name={'name'}></Display>
-        </>
-    );
-}
-
-type TimerProps = { current_time: number; slide_name: string };
-function Display(props: TimerProps) {
-    const current_time = props.current_time;
-    const slide_name = props.slide_name;
     return (
         <div>
-            <p>{current_time}</p>
-            <p>{slide_name}</p>
+            <h4>{currSlideName}</h4>
+            <p>
+                Time to next slide:{' '}
+                {prettyFormatSeconds(currSlideSecondsRemaining)}
+            </p>
+            <p>
+                Total time remaining:{' '}
+                {prettyFormatSeconds(currSecondsRemaining)}
+            </p>
         </div>
     );
-}
+};
+
+/**
+ * Formats time in an MM:SS format
+ * If the number of seconds is beyond an hour, formats in HH:MM:SS instead
+ */
+const prettyFormatSeconds = (seconds: number): string => {
+    if (seconds > 60 * 60) {
+        // include hour timer
+        return new Date(seconds * 1000).toISOString().substring(11, 19);
+    }
+    return new Date(seconds * 1000).toISOString().substring(14, 19);
+};
